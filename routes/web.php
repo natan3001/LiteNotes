f37764465d23508c9afa0123e7b6dbd337860f3b
Natan Martins dos Santos <natantcr@gmail.com>
2022-12-22 11:50:44 -0300

    Tela para mostrar detalhes da nota deletada

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,13 @@ Route::middleware('auth')->group(function () {
 
 Route::resource('/notes', NoteController::class)->middleware(['auth']);
 
-Route::get('/trashed', [TrashedNoteController::class, 'index'])->middleware(['auth'])->name('trashed.index');
+Route::get('/trashed', [TrashedNoteController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('trashed.index');
+
+Route::get('/trashed/{note}', [TrashedNoteController::class, 'show'])
+    ->withTrashed()
+    ->middleware(['auth'])
+    ->name('trashed.show');
 
 require __DIR__ . '/auth.php';
