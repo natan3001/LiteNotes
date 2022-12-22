7d27dbee2b1df65495471724fda07a5200b57257
Natan Martins dos Santos <natantcr@gmail.com>
2022-12-22 11:29:10 -0300

    Criar tela com listagem de notas na lixeira

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Notes') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('trashed.index')" :active="request()->routeIs('trashed.index')">
+                        {{ __('Trash') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -77,6 +82,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('notes.index')" :active="request()->routeIs('notes.index')">
                 {{ __('Notes') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('trashed.index')" :active="request()->routeIs('trashed.index')">
+                {{ __('Trash') }}
             </x-responsive-nav-link>
         </div>
 
--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Notes') }}
+            {{ request()->routeIs('notes.index') ? __('Notes') : __('Trash') }}
         </h2>
     </x-slot>
 
@@ -11,7 +11,9 @@
                 {{ session('success') }}
             </x-alert-success>
 
-            <a href="{{ route('notes.create') }}" class="btn-link btn-lg mb-2">+ New Note</a>
+            @if (request()->routeIs('notes.index'))
+                <a href="{{ route('notes.create') }}" class="btn-link btn-lg mb-2">+ New Note</a>
+            @endif
 
             @forelse ($notes as $note)
                 <div class="my-6 mb-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
@@ -26,7 +28,11 @@
                     </span>
                 </div>
             @empty
-                <p>You have no notes yet.</p>
+                @if (request()->routeIs('notes.index'))
+                    <p>You have no notes yet.</p>
+                @else
+                    <p>No item in the Trash.</p>
+                @endif
             @endforelse
 
             {{ $notes->links() }}
