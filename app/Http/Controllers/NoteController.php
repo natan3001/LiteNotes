7d249a0e6e76abf7d0d9e9d8b22b423c55dbde2e
Natan Martins dos Santos <natantcr@gmail.com>
2022-12-22 10:56:38 -0300

    Mostrar mensagens de retorno ao usuário ao atualizar e deletar nota

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -107,7 +107,7 @@ class NoteController extends Controller
             'text' => $request->text,
         ]);
 
-        return to_route('notes.show', $note);
+        return to_route('notes.show', $note)->with('success', 'Note updated successfully.');
     }
 
     /**
@@ -124,6 +124,6 @@ class NoteController extends Controller
 
         $note->delete();
 
-        return to_route('notes.index');
+        return to_route('notes.index')->with('success', 'Note deleted successfully.');
     }
 }
